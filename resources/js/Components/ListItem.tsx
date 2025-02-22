ba94c9f09f1062046a58319265da31eeb613f570
Lotfi Grira <lotfi.grira@essat-gabes.com>
2025-02-22 12:22:12 +0100

    edit guest layout component

---
+++ resources/js/Components/ListItem.tsx
@@ -0,0 +1,14 @@
+const ListItem = ({ children, NavLink }: any) => {
+    return (
+        <li>
+          <a
+            href={NavLink}
+            className="flex py-2 text-base font-medium text-body-color hover:text-dark dark:text-dark-6 dark:hover:text-white lg:ml-12 lg:inline-flex"
+          >
+            {children}
+          </a>
+        </li>
+    );
+};
+
+export default ListItem;
