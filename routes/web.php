bf7704245e3deedfbf9d65fc5fc941c6cdd98084
Lotfi Grira <lotfi.grira@essat-gabes.com>
2025-02-22 12:24:52 +0100

    clean-up

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,6 @@ use Inertia\Inertia;
 
 Route::get('/', function () {
     return redirect()->route("about");
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
 });
 
 Route::get('/dashboard', function () {
