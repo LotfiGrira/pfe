019b304df71e5e15c27a1d755f6a277af1394737
Lotfi Grira <lotfi.grira@essat-gabes.com>
2025-02-22 12:22:59 +0100

    redirect home route to about route

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
+    return redirect()->route("about");
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
