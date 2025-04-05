728bd42172c774144783c99723660928c12c16ea
Lotfi Grira <lotfi.grira@essat-gabes.com>
2025-04-05 17:48:27 +0200

    add routes for exercice and mirath calculation

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::post('/register', [AuthController::class, 'register']);
+Route::get('/api/exercice/{id}', [ExerciceController::class, 'showJson']);
+Route::post('/resultats', [ExerciceController::class, 'getResultats']);
+
+Route::get('/calcul-mirath', [MirathController::class, 'calculerMirath']);
+
+
+Route::post('/calculate-mirath', [MirathController::class, 'calculateMirath']);
