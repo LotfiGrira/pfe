e31ffe475f6d902757f71e83339d9fa8761041d5
Lotfi Grira <lotfi.grira@essat-gabes.com>
2025-04-05 17:50:36 +0200

    update Guest layout menu links

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -2,17 +2,15 @@ import { PropsWithChildren } from 'react';
 import Navbar from "@/Components/Navbar";
 
 export default function Guest({ children }: PropsWithChildren) {
-    const menu = [{
-        link: "/", text: "الرئيسة"
-    }, {
-        link:"/about", text: "حول"
-        }, {
-        link:"/about", text: "حساب المواربث"
-        }, {
-        link:"/about", text: "اختبارات"
-        }, {
-        link:"/about", text: "مراجع"
-    }];
+    
+    const menu = [
+        { text: "الرئيسة", link: "/" },
+        { text: "حساب المواريث", link: "/calculator" },
+        { text: "اختبارات", link: "/exercice/create" },
+        { text: "مراجع", link: "/references" },
+        
+    ];
+
 
     return (
         <div dir="rtl" className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
@@ -21,6 +19,7 @@ export default function Guest({ children }: PropsWithChildren) {
             <div className="mt-2 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
                 {children}
             </div>
+            
         </div>
     );
 }
