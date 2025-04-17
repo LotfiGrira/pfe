22bd1dff8a6685b3f98046362cc5f3ea3f1b7555
Lotfi Grira <lotfi.grira@essat-gabes.com>
2025-04-17 15:44:47 +0200

    fixation de route

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\ListeHeritierController;
+use App\Http\Controllers\ExamenController;
 
 
 
@@ -17,15 +18,32 @@ Route::get('/', function () {
 
 
 // Routes pour les Questions
-// Page de création d'un exercice
-Route::get('/questions/create',  [QuestionController::class, 'create']);
-Route::post('/questions', [QuestionController::class, 'store'])->name('question.store');
-Route::get('/questions', [QuestionController::class, 'index'])->name('question.index');
-Route::get('/questions/affichage', [QuestionController::class, 'affichage'])->name('question.affichage');
-Route::get('/questions/{id}', [QuestionController::class, 'show'])->name('question.show');
-Route::get('/questions/{id}/edit', [QuestionController::class, 'edit'])->name('question.edit');
-Route::put('/questions/{id}', [QuestionController::class, 'update'])->name('question.update');
-Route::delete('/questions/{id}', [QuestionController::class, 'destroy'])->name('question.destroy');
+
+Route::get('/examens/{examen}/questions/create', [QuestionController::class, 'create'])->name('questions.create');
+Route::post('/examens/{examen}/questions', [QuestionController::class, 'store'])->name('question.store');
+Route::get('/examens/{examen}/questions', [QuestionController::class, 'index'])->name('question.index');
+Route::post('/examens/{examen}/questions', [QuestionController::class, 'store'])
+    ->name('examens.questions.store');
+    Route::get('/examens/{examen}', [QuestionController::class, 'index'])->name('examens.questions.index');
+
+Route::get('/examens/{examen}/questions/affichage', [QuestionController::class, 'affichage'])
+    ->name('question.affichage');
+    Route::get('/examens/{examen}/questions/{id}', [QuestionController::class, 'show'])->name('question.show');
+Route::get('/examens/{examen}/questions/{id}/edit', [QuestionController::class, 'edit'])->name('question.edit');
+Route::put('/examens/{examen}/questions/{id}', [QuestionController::class, 'update'])->name('question.update');
+Route::delete('/examens/{examen}/questions/{id}', [QuestionController::class, 'destroy'])->name('question.destroy');
+
+
+// Routes pour les Examens
+
+Route::get('examens/create', [ExamenController::class, 'create'])->name('examen.create');
+Route::post('/examens', [ExamenController::class, 'store'])->name('examen.store');
+Route::get('/examens', [ExamenController::class, 'index'])->name('examens.index');
+Route::get('/examens/{id}', [ExamenController::class, 'show'])->name('examens.show');
+Route::get('/examens/{id}/edit', [ExamenController::class, 'edit'])->name('examens.edit');
+Route::put('/examens/{id}', [ExamenController::class, 'update'])->name('examens.update');
+Route::delete('/examens/{id}', [ExamenController::class, 'destroy'])->name('examens.destroy');
+
 
 
 Route::get('/dashboard', function () {
@@ -64,25 +82,25 @@ Route::get('/Monasa5atHeritier', function () {
 })->name('monasa5at.heritier');
 
 Route::get('/HamelHeritier', function () {
-    return Inertia::render('HamelHeritier');
+    return Inertia::render('Miraths/HamelHeritier');
 })->name('hamel.heritier');
 Route::get('/WasiyaHeritier', function () {
-    return Inertia::render('WasiyaHeritier');
+    return Inertia::render('Miraths/WasiyaHeritier');
 })->name('wasiya.heritier');
 Route::get('/MafgodHeritier', function () {
-    return Inertia::render('MafgodHeritier');
+    return Inertia::render('Miraths/MafgodHeritier');
 })->name('mafgod.heritier');
 Route::get('/GatelHeritier', function () {
-    return Inertia::render('GatelHeritier');
+    return Inertia::render('Miraths/GatelHeritier');
 })->name('gatel.heritier');
 Route::get('/KaferHeritier', function () {
-    return Inertia::render('KaferHeritier');
+    return Inertia::render('Miraths/KaferHeritier');
 })->name('kafer.heritier');
 Route::get('/tagsim-heritier', function () {
-    return Inertia::render('TagsimHeritier');
+    return Inertia::render('Miraths/TagsimHeritier');
 })->name('tagsim-heritier');
 Route::get('/liste-monasa5at', function () {
-    return Inertia::render('ListeMonasa5at'); 
+    return Inertia::render('Miraths/ListeMonasa5at'); 
 });
 
 Route::get('/listeheritier', [ListeHeritierController::class, 'index'])->name('listeheritier');
