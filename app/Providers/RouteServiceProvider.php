a14102ba7ba5faaf9b7a62945e993d2b1d7888f8
Lotfi Grira <lotfi.grira@essat-gabes.com>
2025-05-02 11:04:09 +0200

    modif RouteServiceProvider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -26,16 +26,13 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+       
         $this->configureRateLimiting();
 
-        $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
-
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
-        });
+    $this->routes(function () {
+        Route::middleware('web')
+            ->group(base_path('routes/web.php'));
+    });
     }
 
     /**
@@ -45,8 +42,15 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function configureRateLimiting()
     {
-        RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
-        });
+        
+    RateLimiter::for('api', function (Request $request) {
+        return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+    });
+
+    // ✅ Définition du rate limiter "admin"
+    RateLimiter::for('admin', function (Request $request) {
+        return Limit::perMinute(100)->by(optional($request->user())->id ?: $request->ip());
+    });
     }
+    
 }
