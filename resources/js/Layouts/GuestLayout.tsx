2c26152454ff77bcdb8d2f030b5d6aaceb978d4a
Lotfi Grira <lotfi.grira@essat-gabes.com>
2025-05-03 14:39:32 +0200

    examen in user auth

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -1,16 +1,25 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren } from "react";
 import Navbar from "@/Components/Navbar";
+import { usePage } from "@inertiajs/react";
 
 export default function Guest({ children }: PropsWithChildren) {
+    const user = usePage().props?.auth?.user;
+
     const menu = [
         { text: "الرئيسة", link: "/" },
         { text: "حساب المواريث", link: "/calculator" },
-        { text: "اختبارات", link: "/examens" },
         { text: "مراجع", link: "/references" },
     ];
 
+    if (user !== null) {
+        menu.push({ text: "اختبارات", link: "/examens/liste" });
+    }
+    
     return (
-        <div dir="rtl" className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
+        <div
+            dir="rtl"
+            className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0"
+        >
             <Navbar menu={menu} />
 
             <div className="mt-2 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
