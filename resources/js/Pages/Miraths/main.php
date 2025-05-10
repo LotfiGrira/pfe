1c967b3cfa2ec14c4d5a6d19cfd02b8d29031cfa
Lotfi Grira <lotfi.grira@essat-gabes.com>
2025-05-10 14:54:20 +0200

    tartib function

---
+++ resources/js/Pages/Miraths/main.php
@@ -0,0 +1,32 @@
+<?php
+class MirathCalculator
+{public function
+    mirathazawj()
+    mirathazawja()
+    mirathalab()
+    mirathalom()
+    mirathaljad() 
+    Mirathaljadah_li_ab()
+    Mirathaljadat_li_om()
+    mirathalbanat()
+    mirathbanat_alabna()
+    mirathabana_albanat()
+    mirathbanat_albanat()
+    mirathalikhwa_li_om()
+    mirathalakhawat_li_om()
+    mirathalakhawat_alashakikat()
+    mirathalakhawat_li_ab()
+   
+
+    
+    mirathalabna()
+    mirathabna_alabna()
+    mirathalikhwa_alashika()
+    mirathalikhwa_li_ab()
+    mirathabna_alikhwa_alashika()
+    mirathabna_alikhwa_li_ab()
+    mirathala3mam_alashika()
+    mirathala3mam_li_ab()
+    mirathabna_ala3mam_alashika()
+    mirathabna_ala3mam_li_ab()
+  }
